2ef9e9b22992f3c61fec08fc13b3120005959682
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-08-22 17:07:31 +0530

    Add files via upload

---
+++ FBS_Work/C_Module/Assignments/Assignments2/GreatestNo.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>=b){
+		if(a>=c){
+			printf("A is greatest");
+		}
+		else{
+			printf("C is greatest");
+		}
+}
+	else{
+		if(b>=c){
+			printf("B is greatest");
+		}
+		else{
+			printf("C is greatest");
+		}
+	}
+	
+}
