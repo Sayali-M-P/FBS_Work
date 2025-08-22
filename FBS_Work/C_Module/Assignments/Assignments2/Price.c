5909d4cea93f61564a44ed7bccb4914799b59fbe
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-08-22 17:08:08 +0530

    Add files via upload

---
+++ FBS_Work/C_Module/Assignments/Assignments2/Price.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int price;
+    char isStudent;
+    float discount;
+
+    printf("Enter price: ");
+    scanf("%d", &price);
+
+    printf("Are you a student? (y/n): ");
+    scanf(" %c", &isStudent);  // Space before %c is important
+
+    if (isStudent == 'y') {
+        if (price > 500) {
+            discount = price * 0.20;
+        } else {
+            discount = price * 0.10;
+        }
+    } else {
+        if (price > 600) {
+            discount = price * 0.15;
+        } else {
+            discount = 0;
+        }
+    }
+
+    printf("Discount: %.2f\n", discount);
+    printf("Final price: %.2f\n", price - discount);
+
+    return 0;
+}
+
+
