678b4be1ad01a5cd56ee843da9e06b25ee60ff84
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-05 17:09:12 +0530

    Update Program 10.c

--- FBS_Work/C_Module/Assignments/Assignments 0/Program 10.c
+++ FBS_Work/C_Module/Assignments/Assignments 0/Program 10.c
@@ -1,14 +1,28 @@
-#include<stdio.h>
-void marks();
-void main(){
-	marks();
+#include <stdio.h>
+int total; // global variable
+
+// Function declarations
+int T_marks();
+int T_per();
+
+int main() {
+    int per;
+
+    total = T_marks();  
+    per = T_per();   // function call here   
+
+    printf("Total Marks: %d\n", total);
+    printf("Percentage: %d%%\n", per);
+} // main ends here
+
+int T_marks() {
+    int s1, s2, s3, s4, s5;
+    printf("Enter marks of 5 subjects: ");
+    scanf("%d%d%d%d%d", &s1, &s2, &s3, &s4, &s5);
+    return s1 + s2 + s3 + s4 + s5; // function defination here
 }
-void marks(){
-	int s1,s2,s3,s4,s5,total,per;
-	printf("Enter 5 subject of marks");
-	scanf("%d%d%d%d%d",&s1,&s2,&s3,&s4,&s5);
-	total=s1+s2+s3+s4+s5;
-	per=total/5;
-	printf("total marks:%d\n",total);
-	printf("Percentage:%d",per,"%");
+
+
+int T_per() {
+    return total / 5;  
 }
