311279488b62d0c53551cad52755efacf622a04a
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-05 17:52:47 +0530

    Update Age.c

--- FBS_Work/C_Module/Assignments/Assignments1/Age.c
+++ FBS_Work/C_Module/Assignments/Assignments1/Age.c
@@ -1,9 +1,19 @@
+#include<stdio.h>
+int age(); // function declaration here
 int main(){
-	int age=16;
-	if(age>18){
+	int check=age(); // function call here
+	if(check==1)
 		printf("Eligible for vote");
-	}
-	else{
+	else
 		printf("Not eligible for vote");
-	}
-}
+} // main ends here
+
+int age(){
+	int age;
+	printf("Enter your age:");
+	scanf("%d",&age);
+	if(age>18)
+		return 1;
+	else
+		return 0;
+} // function defination here
