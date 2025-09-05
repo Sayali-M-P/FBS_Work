10f91fa1d63b52123285bc25fd0d005d57eba65b
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-05 20:08:07 +0530

    Update Divisible.c

--- FBS_Work/C_Module/Assignments/Assignments2/Divisible.c
+++ FBS_Work/C_Module/Assignments/Assignments2/Divisible.c
@@ -1,19 +1,25 @@
-#include<stdio.h>
-int main(){
-	int number;
-	printf("Enter a number");
-	scanf("%d",&number);
-	if(number%3==0){
-		if(number%5==0){
-			printf("Divisible by both");
-		}
-	}
-	else{
-		if(number%5==0){
-			printf("divisible by 5 but not by 3");
-		}
-		else{
-			printf("Divisible by none");
-		}
-	}
-}
+#include <stdio.h>
+int divisible(); // function declaration here 
+int main() {
+    divisible(); // function call here
+    return 0;
+} // main ends here
+
+int divisible() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    if (number % 3 == 0 && number % 5 == 0) {
+        printf("Divisible by both 3 and 5\n");
+    }
+    else if (number % 3 == 0) {
+        printf("Divisible by 3 but not by 5\n");
+    }
+    else if (number % 5 == 0) {
+        printf("Divisible by 5 but not by 3\n");
+    }
+    else {
+        printf("Divisible by none\n");
+    }
+    return 0;
+} // function defination here
