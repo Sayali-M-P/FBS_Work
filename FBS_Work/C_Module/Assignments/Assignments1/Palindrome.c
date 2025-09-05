4a2656b3bb77bf9f900f3b85d545dc767b8e1199
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-05 17:34:59 +0530

    Update Palindrome.c

--- FBS_Work/C_Module/Assignments/Assignments1/Palindrome.c
+++ FBS_Work/C_Module/Assignments/Assignments1/Palindrome.c
@@ -1,12 +1,35 @@
-#include<stdio.h>
-int main(){
-	int number=121;
-	int original=number;
-	int reverse=0;
-	if(number==0){
-		printf("Number is palindrome");
-	}
-	else{
-		printf("Number is not palindrome");
-	}
-}
+#include <stdio.h>
+
+int palindrome();  // Function declaration here
+
+int main() {
+    int rev = palindrome(); // function call here
+
+    if (rev == 1)
+        printf("Number is palindrome\n");
+    else
+        printf("Number is not palindrome\n");
+
+} // main ends here
+
+int palindrome() {
+    int number, original, reverse = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    original = number;
+
+    // Reverse the number
+    while (number != 0) {
+        remainder = number % 10;
+        reverse = reverse * 10 + remainder;
+        number = number / 10;
+    }
+
+    
+    if (original == reverse)
+        return 1;
+    else
+        return 0;
+} // function defination here
