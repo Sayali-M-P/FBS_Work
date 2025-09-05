0fc19da0ee67aa255e3e0fb0e64bf67cbbe60a16
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-05 16:51:52 +0530

    Add files via upload

---
+++ FBS_Work/C_Module/Assignments/Assignments 5/Program 8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int n=4;
+	int i,j;
+	for(i=1;i<=n;i++){
+		for(j=1;j<=i;j++){
+		printf("*");
+        } 
+        printf("\n");
+		
+	}
+	for(i=5;i<=n;i++){
+		for(j=1;j<=i;j++){
+		printf("*");
+	}
+	printf("\n");
+	}
+	
+}
+
