e3931c67bc4bd9d6a36386bcf33d62228344ff4f
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-05 19:54:57 +0530

    Update Marks.c

--- FBS_Work/C_Module/Assignments/Assignments2/Marks.c
+++ FBS_Work/C_Module/Assignments/Assignments2/Marks.c
@@ -1,27 +1,34 @@
 #include<stdio.h>
+int marks(); // function declaration here
 int main(){
+	marks(); // function call here
+	return 0;
+} // main ends here
+
+int marks(){
 	int marks;
+	printf("Enter a marks");
 	scanf("%d",&marks);
 	if(marks>55){
 		if(marks>75){
-			printf("Distinction %d\n",marks);
+			printf("Distinction %d\n");
 		}
 		else{
 			if(marks>65){
-				printf("First class %d\n",marks);
+				printf("First class %d\n");
 			}
 			else{
-				printf("Second class %d\n",marks);
+				printf("Second class %d\n");
 			}
 		}
 	}	
 	else{
 		if(marks>=40){
-			printf("Pass class %d\n",marks);
+			printf("Pass class %d\n");
 		}
 		else{
 			printf("Fail");
 		}
 	}
-	
-}
+	return 0;
+} // function defination here
