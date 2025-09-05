87f0b2f1436765bfa16ca2d7cc3fc3e6abe8e914
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-05 17:26:51 +0530

    Update EvenOrOdd.c

--- FBS_Work/C_Module/Assignments/Assignments1/EvenOrOdd.c
+++ FBS_Work/C_Module/Assignments/Assignments1/EvenOrOdd.c
@@ -1,9 +1,20 @@
+#include<stdio.h>
+int evenodd(); // function declaration here
 int main(){
-	int num=16;
-	if(num%2==0){
+	int res=evenodd(); //function call here
+	if(res==1)
 		printf("No is Even");
-	}
-	else{
-		printf("No is odd");
-	}
-}
+	else
+			printf("No is odd");
+} // main ends here
+
+int	evenodd(){
+		int num;
+		printf("Enter number");
+		scanf("%d",&num);
+	if(num%2==0)
+		return 1;
+	else
+		return 0;
+	
+} // function defination here 
