89d22de76a748454259a8c57e649c7d927da8037
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-05 17:46:19 +0530

    Update VowelOrConst.c

--- FBS_Work/C_Module/Assignments/Assignments1/VowelOrConst.c
+++ FBS_Work/C_Module/Assignments/Assignments1/VowelOrConst.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
+int vowelcon(); // function declaration here
 int main(){
-	char ch='b';
-	if(ch=='a'|| ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+	int result=vowelcon(); // function call here
+	if(result==1)
 		printf("Character is vowel");
-	}
-	else{
+	else
 		printf("Character is constant");
-	}
-}
+} // main ends here
+
+int vowelcon(){
+	char ch='b';
+	if(ch=='a'|| ch=='e' || ch=='i' || ch=='o' || ch=='u')
+		return 1;
+	else
+		return 0;
+} // function defination here
