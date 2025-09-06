171494196cbaabbc1cdeeca598b4281c10290e65
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-06 16:26:56 +0530

    Update Program 5.c

--- FBS_Work/C_Module/Assignments/Assignments 0/Program 5.c
+++ FBS_Work/C_Module/Assignments/Assignments 0/Program 5.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-int average(); // function declaration here
+void average(int,int,int,int,int); // function declaration here
 void main(){
-	int avg;
-	avg=average(); // function call here 
-	printf("Average of 5 numbers is:%d",avg);
-} // main ends here
-
-int average(){
 	int m1,m2,m3,m4,m5;
 	printf("Enter five numbers");
 	scanf("%d%d%d%d%d",&m1,&m2,&m3,&m4,&m5);
-	return (m1+m2+m3+m4+m5)/5;	
+	average(m1,m2,m3,m4,m5); // function call here 
+	
+} // main ends here
+
+void average(int a,int b,int c,int d,int e){
+	int avg=(a+b+c+d+e)/5;	
+	printf("Average of 5 numbers is:%d",avg);
 } // function defination here
