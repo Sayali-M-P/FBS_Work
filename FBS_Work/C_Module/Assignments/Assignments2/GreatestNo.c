b50ca06e92fbbe5572a22cea079aac1fd6f99436
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-07 16:39:40 +0530

    Update GreatestNo.c

--- FBS_Work/C_Module/Assignments/Assignments2/GreatestNo.c
+++ FBS_Work/C_Module/Assignments/Assignments2/GreatestNo.c
@@ -1,29 +1,28 @@
 #include<stdio.h>
-int greatest(); // function declaration here
+void greatest(int,int,int); // function declaration here
 int main(){
-	greatest(); // function call here
-	return 0;
-} // main ends here
-
-int greatest(){
 	int a,b,c;
 	printf("Enter 3 numbers");
 	scanf("%d%d%d",&a,&b,&c);
-	if(a>=b){
-		if(a>=c){
-			printf("A is greatest");
+	greatest(a,b,c); // function call here
+} // main ends here
+
+void greatest(int x,int y,int z){
+	
+	if(x>=y){
+		if(x>=z){
+			printf("x is greatest");
 		}
 		else{
-			printf("C is greatest");
+			printf("z is greatest");
 		}
 }
 	else{
-		if(b>=c){
-			printf("B is greatest");
+		if(y>=z){
+			printf("y is greatest");
 		}
 		else{
-			printf("C is greatest");
+			printf("z is greatest");
 		}
 	}
-	return 0;
 } // function defination here
