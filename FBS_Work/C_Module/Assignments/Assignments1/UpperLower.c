b57fa87e4e992deccb4779fe00a2d3c50f1b859e
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-07 16:24:31 +0530

    Update UpperLower.c

--- FBS_Work/C_Module/Assignments/Assignments1/UpperLower.c
+++ FBS_Work/C_Module/Assignments/Assignments1/UpperLower.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
-int upperlower();  // Function declaration here
+void upperlower(int);  // Function declaration here
 int main() {
-    int check = upperlower();  //  function call here
-    if(check == 1)
+	char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);  
+    upperlower(ch);  //  function call here
+    
+} // main ends here
+
+void upperlower(int c) {
+    if(c >= 'A' && c <= 'Z')
         printf("Character is uppercase.\n");
     else
         printf("Character is lowercase.\n");
-} // main ends here
-
-int upperlower() {
-    char ch;
-    printf("Enter a character: ");
-    scanf(" %c", &ch);  
-    if(ch >= 'A' && ch <= 'Z')
-        return 1;
-    else
-        return 0;
 } // function defination here 
-
