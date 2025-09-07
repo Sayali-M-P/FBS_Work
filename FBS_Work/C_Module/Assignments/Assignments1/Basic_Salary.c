b3d01aef7c28bd429709658bd5dd1e139fff8079
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-07 16:28:07 +0530

    Update Basic_Salary.c

--- FBS_Work/C_Module/Assignments/Assignments1/Basic_Salary.c
+++ FBS_Work/C_Module/Assignments/Assignments1/Basic_Salary.c
@@ -1,26 +1,25 @@
 #include<stdio.h>
-int salary(); // function declaration here
+void salary(int); // function declaration here
 int main(){
-	float total;
-	total=salary(); // function call here
-	printf("total salary is:%.2f\n",total);
-} // main ends here
-
-int salary(){
 	float basic;
 	printf("Enter basic salary");
 	scanf("%f",&basic);
+	salary(basic); // function call here
+} // main ends here
+
+void salary(int bs){
 	float da,ta,hra,total;
-	if(basic<=5000){
-		da=basic*0.10;
-		ta=basic*0.20;
-		hra=basic*0.25;
+	if(bs<=5000){
+		da=bs*0.10;
+		ta=bs*0.20;
+		hra=bs*0.25;
 	}
 	else{
-		da=basic*0.15;
-		ta=basic*0.25;
-		hra=basic*0.30;
+		da=bs*0.15;
+		ta=bs*0.25;
+		hra=bs*0.30;
 	}
-	total=basic+da+ta+hra;
-	return total;
+	total=bs+da+ta+hra;
+	printf("total salary is:%.2f\n",total);
+	
 } // function defination here
