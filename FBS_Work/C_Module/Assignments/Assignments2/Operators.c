a472a136b4312533295329f2ba68ea270132086d
Sayali Pavashe <sayalipavashe4@gmail.com>
2025-09-07 16:33:20 +0530

    Update Operators.c

--- FBS_Work/C_Module/Assignments/Assignments2/Operators.c
+++ FBS_Work/C_Module/Assignments/Assignments2/Operators.c
@@ -1,37 +1,36 @@
 #include<stdio.h>
-int arithmatic(); // function declaration here
+void arithmatic(int,int,char); // function declaration here
 int main(){
-	arthmatic(); // function call here
-	return 0;
-} // main ends here
-
-int arthmatic(){
 	int x,y;
 	char op;
 	printf("Enter 2 numbers");
 	scanf("%d%d",&x,&y);
 	printf("Enter a operator");
 	scanf(" %c",&op);
-	if(op =='+'){
-		printf("Addition:%d", x+y);
+	arithmatic(x,y,op); // function call here
+} // main ends here
+
+void arithmatic(int a,int b,char opp){
+	if(opp =='+'){
+		printf("Addition:%d", a+b);
 	}
-	else if(op == '-'){
-		printf("Substraction:%d", x-y);
+	else if(opp == '-'){
+		printf("Substraction:%d", a-b);
 	}
-	else if(op == '*'){
-		printf("Multiplication:%d",x*y);
+	else if(opp == '*'){
+		printf("Multiplication:%d",a*b);
 	}
-	else if(op == '/'){
-		if(y!=0){
-			printf("Division:%d",x/y);
+	else if(opp == '/'){
+		if(b!=0){
+			printf("Division:%d",a/b);
 		}
 		else{
 			printf("Divisible by zero");
 		}
 	}
-	else if(op == '%'){
-		if(y!=0){
-			printf("Modulus:%d",x%y);
+	else if(opp == '%'){
+		if(b!=0){
+			printf("Modulus:%d",a%b);
 		}
 		else{
 			printf("Error for divisible by zero");
